4ecb9e113b6da92cee58571714d81cda884ccd2b
nocturnal_hum <thanin.chan@gmail.com>
2024-01-31 14:54:25 -0500

    Debug: 'force-dynamic` for page and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Container from './components/Container';
 import HeroBanner from './components/HeroBanner';
 import { products } from '@/utils/products';
