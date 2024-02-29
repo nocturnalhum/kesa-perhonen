eafc1464d5ea0e9e66a45a47add004a486c3cc6e
nocturnal_hum <thanin.chan@gmail.com>
2024-02-29 13:10:04 -0500

    Implement links to product details for items in Order Details

--- app/order/[orderId]/OrderItem.tsx
+++ app/order/[orderId]/OrderItem.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
 import Image from 'next/image';
 import { CartProductType } from '@prisma/client';
 import { truncateText } from '@/utils/truncateText';
 import { formatPrice } from '@/utils/formatPrice';
+import { useRouter } from 'next/navigation';
 
 interface OrderItemProps {
   item: CartProductType;
 }
 const OrderItem: React.FC<OrderItemProps> = ({ item }) => {
+  const router = useRouter();
+
   const discountPrice =
     item.selectedItem.itemDetail.price *
     (1 - item.selectedItem.itemDetail.discount / 100);
-  console.log('ITEM', item);
+
   return (
     <div className='grid grid-cols-5 text-xs md:text-sm gap-4 border-t-[1.5px] border-slate-200 py-4 items-center capitalize'>
       <div
-        className='col-span-2 justify-self-start flex gap-2 md:ga4
-      '
+        onClick={() => router.push('/product/' + item.id)}
+        className='col-span-2 justify-self-start flex gap-2 md:gap-4 cursor-pointer'
       >
         <div className='relative w-[70px] aspect-square'>
           <Image
